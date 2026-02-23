0d1ed32a0dffec88303fd2fd1145a2a9ceaa4a09
= <dominiquefarias21@gmail.com>
2026-02-23 22:44:31 +0100

    .

--- Proyectos python/012-frankestein.py
+++ Proyectos python/012-frankestein.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter import ttk
 import mysql.connector
+
 conexion = mysql.connector.connect(host="localhost",user="empresadam",password="Empresadam123$",database="empresadam")
 cursor = conexion.cursor()
 ventana = tk.Tk()
